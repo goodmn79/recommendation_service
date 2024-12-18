195688702a5ad366350ab017f5ad196dc171e6f5
goodmn <goodmn79@gmail.com>
2024-12-18 13:30:23 +0300

    feature: added layers controller, service, repository

---
+++ src/main/java/pro/sky/recommendation_service/repository/RecommendationRepository.java
@@ -0,0 +1,22 @@
+package pro.sky.recommendation_service.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import pro.sky.recommendation_service.model.Recommendation;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class RecommendationRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Recommendation> mapper;
+
+    Optional<Recommendation> findByName(String name) {
+        String query = "SELECT * FROM RECOMMENDATIONS WHERE NAME = ?";
+        return Optional.ofNullable(jdbcTemplate.queryForObject(query, mapper, name));
+    }
+}
+
